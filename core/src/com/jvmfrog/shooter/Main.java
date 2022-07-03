a4502672e70e1336af1adc3d3a28a1f339589fa3
Kirill <kirbo06@mail.ru>
2022-07-03 17:06:18 +0200

    added readme & shiza

--- core/src/com/jvmfrog/shooter/Main.java
+++ core/src/com/jvmfrog/shooter/Main.java
@@ -1,31 +1,17 @@
 package com.jvmfrog.shooter;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class Main extends Game {
+    @Override
+    public void create() {
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(1, 1, 1, 1);
+        super.render();
+    }
 }
