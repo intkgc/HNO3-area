55cdb3df49e5c7a1d78ef86c92a7321123463692
intbyte-100 <5656ltl@gmail.com>
2022-07-03 18:28:09 +0300

    Add fields for tile

---
+++ core/src/com/jvmfrog/shooter/render/Render.java
@@ -0,0 +1,10 @@
+package com.jvmfrog.shooter.render;
+
+import com.jvmfrog.shooter.world.World;
+
+
+public class Render {
+    private World world;
+
+
+}
--- core/src/com/jvmfrog/shooter/world/Tile.java
+++ core/src/com/jvmfrog/shooter/world/Tile.java
@@ -1,4 +1,8 @@
 package com.jvmfrog.shooter.world;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
 public class Tile {
+    public Sprite sprite;
+    boolean isUpperLayer;
 }
